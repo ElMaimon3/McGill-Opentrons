008563482b4583a3c51d49e86a75404dc94cdc77
GabrielS <97903747+gabeN000@users.noreply.github.com>
2023-12-21 17:30:16 -0600

    Added description to Gibson Assembly protocol

--- gibsonassembly.py
+++ gibsonassembly.py
@@ -4,6 +4,7 @@ from opentrons import protocol_api
 # metadata
 metadata = {
     'protocolName': 'Gibson Assembly',
+    'description': 'Opentrons protocol for Gibson Assembly (OT-2)',
 }
 requirements = {"robotType": "OT-2", "apiLevel": "2.15"}
 def run(protocol: protocol_api.ProtocolContext):
