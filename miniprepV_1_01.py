aaf549408f401fcbf3389cf74208d4866e6604b3
GabrielS <97903747+gabeN000@users.noreply.github.com>
2024-02-19 14:36:25 -0500

    deleted unused pipette definition

--- miniprepV_1_01.py
+++ miniprepV_1_01.py
@@ -26,7 +26,6 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # Load pipettes
     p300 = protocol.load_instrument('p300_single_gen2', 'left', tip_racks=[protocol.load_labware('opentrons_96_tiprack_300ul', '5')])
-    p20 = protocol.load_instrument('p20_multi_gen2', 'right', tip_racks=[protocol.load_labware('opentrons_96_filtertiprack_20ul', '10')])
 
     # Define sample locations on the 96-well plate
     num_samples = 1
