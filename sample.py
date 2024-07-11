e4afdfb82c7582d2e9f792d985906d98938a642b
Gabriel Straface <37457460+ElMaimon3@users.noreply.github.com>
2024-07-11 16:07:05 -0400

    Added more folders

---
+++ sample.py
@@ -0,0 +1,16 @@
+# imports
+from opentrons import protocol_api
+
+# metadata
+metadata = {
+    'protocolName': '',
+    "author": "",
+    'description': '',
+}
+requirements = {"robotType": "OT-2", "apiLevel": "2.15"}
+def run(protocol: protocol_api.ProtocolContext):
+     # labware
+
+     # pipettes
+     # loaction definitions
+     # commands
