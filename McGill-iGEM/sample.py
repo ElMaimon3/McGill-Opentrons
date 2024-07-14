4d3518df142545a7b5115d65ba68e1d655930b9c
Gabriel Straface <37457460+ElMaimon3@users.noreply.github.com>
2024-07-14 12:42:42 -0400

    Added runtime parameters to smaple

--- McGill-iGEM/sample.py
+++ McGill-iGEM/sample.py
@@ -8,6 +8,25 @@ metadata = {
     'description': '',
 }
 requirements = {"robotType": "OT-2", "apiLevel": "2.19"}
+
+# Runtime Parameters (Recommended)
+def add_parameters(parameters: protocol_api.Parameters):
+    parameters.add_int(
+        variable_name = "some_number",
+        display_name = "Some Number",
+        description = "",
+        default = 40,
+        minimum = 20,
+        maximum = 50,
+        unit = "µL"
+    )
+    parameters.add_bool(
+        variable_name = "debug",
+        display_name = "Debugging Mode",
+        description = "",
+        default = False
+    )
+
 def run(protocol: protocol_api.ProtocolContext):
      # labware
     tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', '1')
@@ -18,3 +37,11 @@ def run(protocol: protocol_api.ProtocolContext):
      # loaction definitions
 
      # commands
+    some_number = protocol.params.some_number
+    if  not protocol.params.debug:
+        # Protocol goes here
+        pass
+    else:
+        #Debugging code goes here
+        pass
+    
