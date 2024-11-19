84e73911812042296410a3925bd557fcfa408da4
Gabriel Straface <37457460+ElMaimon3@users.noreply.github.com>
2024-11-19 13:06:10 -0500

    Flex PCR improvments

--- Ignea-Lab/Flex/PCR copy.py
+++ Ignea-Lab/Flex/PCR copy.py
@@ -11,31 +11,8 @@ metadata = {
     with template DNA pre loaded on the PCR plate. Depending on the use 
     case, primers have to be added to each sample or left in the reservoir'''
 }
-requirements = {"robotType": "Flex", "apiLevel": "2.19"}
+requirements = {"robotType": "Flex", "apiLevel": "2.20"}
 
-def add_locations(bytes):
-    # Define sample locations by column, row and/or well in a csv file
-    sample_columns = [] # eg. ['1', '2']
-    sample_rows = [] # eg. ['A', 'B']
-    sample_wells = [] # eg. ['A1', 'B1']
-    # Decode the bytes object into a string
-    csv_string = bytes.decode('utf-8')
-
-    # Use io.StringIO to create a file-like object for csv.reader
-    csv_file = io.StringIO(csv_string)
-    reader = csv.reader(csv_file)
-    header = next(reader)
-    header = next(reader)
-    for row in reader:
-        for i in range(3):
-           if len(row[i]) != 0:
-                if i == 0:
-                    sample_columns.append(row[i])
-                elif i == 1:
-                    sample_rows.append(row[i])
-                elif i == 2:
-                    sample_wells.append(row[i])
-    return sample_columns, sample_rows, sample_wells
 
 # Runtime Parameters
 def add_parameters(parameters: protocol_api.Parameters):
@@ -195,9 +172,6 @@ def add_parameters(parameters: protocol_api.Parameters):
     )
 
 def run(protocol: protocol_api.ProtocolContext):
-    bundled_data = protocol.bundled_data['PCR_locations.csv']
-    sample_columns, sample_rows, sample_wells = add_locations(bundled_data)
-
 
     # PCR parameters
     mode = protocol.params.location_mode
@@ -237,6 +211,8 @@ def run(protocol: protocol_api.ProtocolContext):
     pcr_volume = sample_volume + master_mix_volume + primer_volume
     master_mix = res.wells_by_name()['A1'].top(-34)
     primers = res.wells_by_name()['A2'].top(-34)
+    p1 = 7.5
+    mm1 = 7.5
 
     # Thermocycling program definition
     pcr_program = [
@@ -247,78 +223,62 @@ def run(protocol: protocol_api.ProtocolContext):
     # Commands
     tc_mod.open_lid()
 
-    # Define wells and remove duplicates
-    destination_wells = []
-    for col in sample_columns:
-       destination_wells.extend(tc_plate.columns_by_name()[col])
-    for row in sample_rows:
-       destination_wells.extend(tc_plate.rows_by_name()[row])
-    destination_wells.extend([tc_plate.wells_by_name()[well] for well in sample_wells])
-
-    # Initialize an empty dictionary to track occurrences
-    occurrences = {}
-    # Initialize an empty list to store the unique wells
-    unique_wells = []
-    for well in destination_wells:
-       # Convert the well object to a string to use it as a dictionary key
-       well_str = str(well)
-       if well_str not in occurrences:
-           # If the well is not in the dictionary, add it to unique_wells
-           unique_wells.append(well)
-           # And add it to the dictionary
-           occurrences[well_str] = True
-       else:
-            # If the well is already in the dictionary, it's a duplicate
-            protocol.comment(f"Duplicate location found and removed: {well_str}")
-    # Replace destination_wells with the list of unique wells
-    destination_wells = unique_wells
-    num_samples = len(destination_wells)
-
-
-    # Transfer appropriate reagents to pcr plate
-    if not primers_loaded:
-        if primer_volume < 50:
-            primer_pipette = p50
-        else:
-            primer_pipette = p200
-        primer_pipette.pick_up_tip()
-        for well in destination_wells:
-            if p1 >= primer_volume:
-                primer_pipette.aspirate(primer_volume,primers)
-                primer_pipette.dispense(primer_volume,well.top())
-                p1 -= primer_volume
+    if mode == 'simple':
+        # Transfer appropriate reagents to pcr plate
+        if not primers_loaded:
+            if primer_volume < 50:
+                primer_pipette = p50
             else:
-                primer_pipette.aspirate(primer_volume,primers2)
-                primer_pipette.dispense(primer_volume,well.top())
-        primer_pipette.drop_tip()
+                primer_pipette = p200
+            primer_pipette.pick_up_tip()
+            for c in tc_plate.columns[:cols]:
+                primer_pipette.aspirate(primer_volume,primers.top(-vol_to_height(p1)))
+                primer_pipette.dispense(primer_volume,c.top())
+                p1 -= 8 * 0.001 * primer_volume
 
-    if master_mix_volume <50:
-        master_pipette = p50
-    else:
-        master_pipette = p200
+            primer_pipette.drop_tip()
 
-    master_pipette.pick_up_tip()
-    for well in destination_wells:
-        if mm1 >= master_mix_volume:
-            master_pipette.mix(1,20,master_mix)
-            master_pipette.aspirate(master_mix_volume, master_mix)
-            master_pipette.dispense(master_mix_volume, well.top())
-            mm1 -= master_mix_volume
-        elif mm2 >= master_mix_volume:
-            master_pipette.mix(1,20,master_mix2)
-            master_pipette.aspirate(master_mix_volume, master_mix2)
-            master_pipette.dispense(master_mix_volume, well.top())
-            mm2 -= master_mix_volume
-        elif mm3 >= master_mix_volume:
-            master_pipette.mix(1,20,master_mix3)
-            master_pipette.aspirate(master_mix_volume, master_mix3)
-            master_pipette.dispense(master_mix_volume, well.top())
-            mm3 -= master_mix_volume   
+        if master_mix_volume < 50:
+            master_pipette = p50
         else:
-            master_pipette.mix(1,20,master_mix4)
-            master_pipette.aspirate(master_mix_volume, master_mix4)
-            master_pipette.dispense(master_mix_volume, well.top())                     
-    master_pipette.drop_tip()
+            master_pipette = p200
+
+        master_pipette.pick_up_tip()
+        for c in tc_plate.columns[:cols]:
+            master_pipette.mix(1,master_mix_volume,master_mix)
+            master_pipette.aspirate(master_mix_volume, master_mix.top(-vol_to_height(mm1)))
+            master_pipette.dispense(master_mix_volume, well.top())
+            mm1 -= 8 * 0.001 * master_mix_volume                     
+        master_pipette.drop_tip()
+    else:
+        bundled_data = protocol.bundled_data['PCR_locations.csv']
+        sample_columns, sample_rows, sample_wells = add_locations(bundled_data)
+        # Define wells and remove duplicates
+        destination_wells = []
+        for col in sample_columns:
+           destination_wells.extend(tc_plate.columns_by_name()[col])
+        for row in sample_rows:
+           destination_wells.extend(tc_plate.rows_by_name()[row])
+        destination_wells.extend([tc_plate.wells_by_name()[well] for well in sample_wells])
+
+        # Initialize an empty dictionary to track occurrences
+        occurrences = {}
+        # Initialize an empty list to store the unique wells
+        unique_wells = []
+        for well in destination_wells:
+           # Convert the well object to a string to use it as a dictionary key
+           well_str = str(well)
+           if well_str not in occurrences:
+               # If the well is not in the dictionary, add it to unique_wells
+               unique_wells.append(well)
+               # And add it to the dictionary
+               occurrences[well_str] = True
+           else:
+                # If the well is already in the dictionary, it's a duplicate
+                protocol.comment(f"Duplicate location found and removed: {well_str}")
+        # Replace destination_wells with the list of unique wells
+        destination_wells = unique_wells
+        num_samples = len(destination_wells)
 
 
     # Run thermocycler
@@ -330,9 +290,38 @@ def run(protocol: protocol_api.ProtocolContext):
     tc_mod.set_block_temperature(temperature=denaturation_temp,hold_time_seconds= initial_denaturation_time_seconds, block_max_volume=pcr_volume) # Initial denaturation
     tc_mod.execute_profile(steps=pcr_program, repetitions=num_cycles, block_max_volume=pcr_volume)
     tc_mod.set_block_temperature(temperature=extension_temp, hold_time_seconds= final_extension_time_seconds, block_max_volume=pcr_volume) # Final extension
-    tc_mod.set_block_temperature(4)
     tc_mod.deactivate_lid()
     tc_mod.open_lid()
+    tc_mod.set_block_temperature(4)
     
 
+def vol_to_height(vol):
+    full_depth = 40
+    if vol > 0:
+        return round(-2.6*vol + full_depth)
+    else:
+        return full_depth
 
+def add_locations(bytes):
+    # Define sample locations by column, row and/or well in a csv file
+    sample_columns = [] # eg. ['1', '2']
+    sample_rows = [] # eg. ['A', 'B']
+    sample_wells = [] # eg. ['A1', 'B1']
+    # Decode the bytes object into a string
+    csv_string = bytes.decode('utf-8')
+
+    # Use io.StringIO to create a file-like object for csv.reader
+    csv_file = io.StringIO(csv_string)
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    header = next(reader)
+    for row in reader:
+        for i in range(3):
+           if len(row[i]) != 0:
+                if i == 0:
+                    sample_columns.append(row[i])
+                elif i == 1:
+                    sample_rows.append(row[i])
+                elif i == 2:
+                    sample_wells.append(row[i])
+    return sample_columns, sample_rows, sample_wells
