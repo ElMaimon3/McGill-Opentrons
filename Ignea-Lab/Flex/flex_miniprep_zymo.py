b8c76078521a9daf05ca570ca791c6c7b47906cf
Gabriel Straface <37457460+Gabo-st@users.noreply.github.com>
2025-06-17 11:25:24 -0400

    small tweaks

--- Ignea-Lab/Flex/flex_miniprep_zymo.py
+++ Ignea-Lab/Flex/flex_miniprep_zymo.py
@@ -349,8 +349,8 @@ def run(protocol: protocol_api.ProtocolContext):
     p50 = protocol.load_instrument('flex_8channel_50', 'left', tip_racks=[
         protocol.load_labware('opentrons_flex_96_tiprack_50ul', 'C3')
     ])
-    p300 = protocol.load_instrument('flex_8channel_1000', 'right', tip_racks=[
-        protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'B3'),
+    p200 = protocol.load_instrument('flex_8channel_1000', 'right', tip_racks=[
+        protocol.load_labware('opentrons_flex_96_tiprack_200ul', 'B3'),
     ])
     
     # Parse CSV data for well locations
